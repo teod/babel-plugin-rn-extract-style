36ab403e099386605e707cdb785fde5b33ffee9f
Teodor Druta <teodruta@gmail.com>
2020-11-06 17:09:08 +0200

    Add testcase

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,6 +8,7 @@ const entries = [
   "<Text style={[styles.text, { color: 'red' }]}>Big brown fox</Text>;const styles = StyleSheet.create({ text: { fontSize: 20 } });",
   "<Text style={{ color: 'red' }}>Big brown fox</Text>;const customStyles = StyleSheet.create({});",
   "<Text style={{ color: 'red' }}>Big brown fox</Text>;const styles = StyleSheet.create();",
+  "<Text style={{ color: 'red' }}>Big brown fox</Text>;",
 ]
 
 const expected = [
@@ -15,6 +16,7 @@ const expected = [
   'import { StyleSheet } from "react-native";<Text style={[styles.text, styles.Text_style]}>Big brown fox</Text>;const styles = StyleSheet.create({ text: { fontSize: 20 }, Text_style: { color: \'red\' }});',
   'import { StyleSheet } from "react-native";<Text style={customStyles.Text_style}>Big brown fox</Text>;const customStyles = StyleSheet.create({ Text_style: { color: \'red\' }});',
   'import { StyleSheet } from "react-native";<Text style={styles.Text_style}>Big brown fox</Text>;const styles = StyleSheet.create({ Text_style: { color: \'red\' }});',
+  'import { StyleSheet } from "react-native";<Text style={babelGeneratedStyles.Text_style}>Big brown fox</Text>;const babelGeneratedStyles = StyleSheet.create({ Text_style: { color: \'red\' }});',
 ]
 
 test('plugin', (t) => {
